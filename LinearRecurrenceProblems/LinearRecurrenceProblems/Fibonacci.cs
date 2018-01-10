717d0e0c2de943b8dc84f4202a2c0ab1939ef3be
Luke Selbeck <luke.selbeck@allyis.com>
2018-01-09 16:21:57 -0800

    fibonacci methods

---
+++ LinearRecurrenceProblems/LinearRecurrenceProblems/Fibonacci.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace LinearRecurrenceProblems
+{
+	/// <summary>
+	/// Takes int n as user input, outputs the nth fibonacci number.
+	/// </summary>
+	class Fibonacci
+	{
+		static void Main()
+		{
+			while (true)
+			{
+				int n = int.Parse(Console.ReadLine());
+				if (n < 1) throw new ArgumentOutOfRangeException(nameof(n), "N cannot be negative.");
+				const int iterations = 5;
+
+				//long result1;
+				//var t = new Thread(() =>
+				//{
+				//	result1 = GetFibMatrixRecursive(n);
+				//});
+				//t.Start();
+
+				Console.WriteLine($"Matrix method with recursive power method: {GetFibMatrixRecursive(n)}");
+				Console.WriteLine($"Benchmark (ticks): {Benchmark(() => GetFibMatrixRecursive(n), iterations)}\n");
+
+				Console.WriteLine($"Matrix method with eigen power method: {GetFibMatrixConstant(n):0.#}");
+				Console.WriteLine($"Benchmark (ticks): {Benchmark(() => GetFibMatrixConstant(n), iterations)}\n");
+
+				Console.WriteLine($"Simple recursion: {GetFibRecursive(n)}");
+				Console.WriteLine($"Benchmark (ticks): {Benchmark(() => GetFibRecursive(n), iterations)}\n");
+			}
+		}
+
+		public static long GetFibMatrixRecursive(long n)
+		{
+			long[,] transform = { { 0, 1 }, { 1, 1 } };
+			long[,] baseCases = { { 1 }, { 1 } };
+
+			return MatrixHelper.Multiply(MatrixHelper.PowerRecursive(transform, n - 1), baseCases)[0, 0];
+		}
+
+		public static long GetFibRecursive(long n)
+		{
+			if (n < 1) return 0;
+			if (n == 1 || n == 2) return 1;
+			return GetFibRecursive(n - 1) + GetFibRecursive(n - 2);
+		}
+
+		/// <summary>
+		/// This equation is derived from the eigenvalue method of matrix powers:
+		/// T^n = P * D^n * P^(-1)
+		/// Where T is the transformation matrix.
+		/// Only accurate when n is less than 70, due to imprecise floating points
+		/// </summary>
+		public static double GetFibMatrixConstant(long n)
+		{
+			double sqrt5 = Math.Sqrt(5);
+			double fib = Math.Pow(2, -1 - n) * Math.Pow(1 - sqrt5, n) * (1 + sqrt5) / sqrt5
+						- Math.Pow((1 - sqrt5) / 2.0, n) * (1 + sqrt5) / (sqrt5 * (-1 + sqrt5))
+						+ (-1 + sqrt5) * Math.Pow(1 + sqrt5, -1 + n) / (Math.Pow(2, n) * sqrt5) +
+						 Math.Pow(2, -1 - n) * (-1 + sqrt5) * Math.Pow(1 + sqrt5, n) / sqrt5;
+			return Math.Round(fib);
+		}
+
+		private static long Benchmark(Action act, int iterations)
+		{
+			GC.Collect();
+			act.Invoke(); // run once outside of loop to avoid initialization costs
+			Stopwatch sw = Stopwatch.StartNew();
+			for (int i = 0; i < iterations; i++)
+			{
+				act.Invoke();
+			}
+			sw.Stop();
+			return sw.ElapsedTicks / iterations;
+		}
+	}
+}
---
+++ LinearRecurrenceProblems/LinearRecurrenceProblems/MatrixHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace LinearRecurrenceProblems
+{
+	/// <summary>
+	/// A 2x2 or 3x3 matrix.
+	/// </summary>
+	public static class MatrixHelper
+	{
+		private static readonly long[,] i3x3 = { { 1, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1 } };
+		private static readonly long[,] i2x2 = { { 1, 0 }, { 0, 1 } };
+
+		public static long[,] Multiply(long[,] a, long[,] b)
+		{
+			long[,] c = new long[a.GetLength(0), b.GetLength(1)];
+			for (int i = 0; i < c.GetLength(0); i++)
+			{
+				for (int j = 0; j < c.GetLength(1); j++)
+				{
+					c[i, j] = 0;
+					for (int k = 0; k < a.GetLength(1); k++) // OR k<b.GetLength(0)
+						c[i, j] = c[i, j] + a[i, k] * b[k, j];
+				}
+			}
+			return c;
+		}
+
+		public static long[,] PowerRecursive(long[,] a, long power)
+		{
+			if (a.GetLength(0) != a.GetLength(1)) throw new ArgumentException("Matrix must be square");
+			if (power == 0) return a.GetLength(0) == 2 ? i2x2 : i3x3;
+			if (power == 1) return a;
+			if (power % 2 != 0) return Multiply(a, PowerRecursive(a, power - 1));
+			var half = PowerRecursive(a, power / 2);
+			return Multiply(half, half);
+		}
+	}
+}
